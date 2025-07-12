772d74472c99fad6808ca8e2c6172cd807ea621d
Ismaël Lajaaiti <ismael.lajaaiti@gmail.com>
2025-07-12 18:39:51 +0200

    Add test casc. extinctions

--- test/test-robustness.jl
+++ test/test-robustness.jl
@@ -1,8 +1,17 @@
 @testset "Secondary extinctions" begin
+    # Trophic chain - two species.
     fw = Foodweb([2 => 1]) # 2 eats 1.
     m = default_model(fw)
     sol = simulate(m, [1, 1], 1_000)
     B0 = sol.u[end]
     @test secondary_extinctions(m, 1, B0) == [2]
     @test secondary_extinctions(m, 2, B0) == []
+    # Trophic chain - three species.
+    fw = Foodweb([3 => 2, 2 => 1])
+    m = default_model(fw)
+    sol = simulate(m, [1, 1, 1], 1_000)
+    B0 = sol.u[end]
+    @test secondary_extinctions(m, 1, B0) == [2, 3]
+    @test secondary_extinctions(m, 2, B0) == [3]
+    @test secondary_extinctions(m, 3, B0) == []
 end
